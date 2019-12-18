38dacf03719693750be8bc0ea69669115e5d12f7
Emon9426 <zhang.jian@aiesec.net>
2019-12-18 19:53:18 +0800

    20191218-添加todomvc课程

--- 01_Angular/00_Memo/06_Memo_app.component.ts
+++ 01_Angular/00_Memo/06_Memo_app.component.ts
@@ -8,13 +8,14 @@ import { Component } from '@angular/core';
 
 @Component({                             // → 组件的装饰器，用于修饰组件的特性
   selector: 'app-root',                  // → 用于定义组件渲染的标签名称，需要和Index.html中的程序出口标签保持一致
-  templateUrl: './app.component.html',   // → 用于指定组件的模板文件
+  templateUrl: './app.component.html',   // → 用于指定组件的模板文件，也可以使用内联模板
   styleUrls: ['./app.component.css']     // → 一个数组，用于存放组件相关的样式文件
 })
 export class AppComponent {
   // 组件相关的内容数据
   title = 'my-app';
   count = 0;
+  //完整的写法：public title: string = 'my-app';
   // 组件中的方法
   increment = function () {
     this.count++; // 使用this关键字访问组件内的资源
